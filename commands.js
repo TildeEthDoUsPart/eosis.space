a81359d8dc7d86630c4eedb6986125f7b32c5310
Antoine Cerdan <antoinecerdan31@gmail.com>
2023-11-22 17:14:34 +0100

    no more <br>s and commands parser finished ?

--- commands.js
+++ commands.js
@@ -1,10 +1,8 @@
 error = [
-    "<br>",
-    "Command unrecongized. Try help for a list of commands"
+    "Command not found. Type 'help' to get a list of all available commands."
 ]
 
 about = [
-    "<br>",
     "hello",
     "im <b> eosis </b>",
     "cool",
@@ -13,18 +11,25 @@ about = [
 
 
 cat = [
-    "<br>",
     `&nbsp/\\_/\\ `,
-    `( o.o ) `,
+    `( o.o ) &nbsp&nbsp < meow :3`,
     `&nbsp> ^ < `
 ]
 
 motd = [
+    `&nbsp|\\_/| &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp_`,
+    `&nbsp(. .) &nbsp&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp___ &nbsp____  &nbsp_____(_)____`,
+    `&nbsp&nbsp=w= (\\ &nbsp&nbsp&nbsp  &nbsp/ _ \\/ __ \\/ ___/ / ___/`,
+    `&nbsp/ ^ \\// &nbsp&nbsp&nbsp / &nbsp__/ /_/ (__  &nbsp) (__ &nbsp) `,
+    `(|| ||) &nbsp&nbsp&nbsp&nbsp \\___/\\____/____/_/____/  `,
+    `,""_""_ .  `,
+    "Welcome to my terminal !",
+    "Type 'help' to get a list of all available commands.",
+    "---",
     "<br>",
-    `&nbsp|\\_/| `,
-    `&nbsp(. .) `,
-    `&nbsp&nbsp=w= (\\ `,
-    `&nbsp/ ^ \\// `,
-    `(|| ||) `,
-    `,""_""_ . `
+    "This site is open-source ! Type the 'repo' command to get to the repository"
+]
+
+test = [
+    "<span>idk bro</span>"
 ]
--- main.js
+++ main.js
@@ -4,12 +4,14 @@ let commands = {
     "cat" : cat,
     "motd" : motd,
     "projects" : "github",
+    "error" : error,
+    "test" : test
 }
 
 
 let commandBox = $('#commandBox')
 let commandDisplay = $('#commandDisplay')
-let commandPrefix = '<label id="commandPrefix">jsp@eosis.space$ </label>'
+let commandPrefix = '<label id="commandPrefix">jsp@eosis.space:~$ </label>'
 
 let commandHistory = []
 
@@ -17,33 +19,38 @@ let commandHistory = []
 commandBox.on("keypress",function(evt) {
     if (evt.keyCode == 13){
         console.log("tg")
-        submitCommand(commandBox.val())
+        commandHistory.push(commandBox.val())
+        console.log(commandHistory[commandHistory.length-1] + "will eb setn to val")
+        submitCommand(commandBox.val(),commandHistory[commandHistory.length-1])
     }
 })
 
 
-function submitCommand(command){
+function submitCommand(command,value){
     commandBox.val("")
     console.log(command in commands)
     if (command in commands){
-        print_command(command)
+        print_command(command,value)
         prepare_commandBox()
     } else {
-        commandDisplay.append(commandPrefix + command)
-        error.map((line) => {commandDisplay.append(line + "<br>")})
+        print_command("error",value)
         prepare_commandBox()
     }
 }
 
-function print_command(command){
-    commandDisplay.append(commandPrefix + command)
+function print_command(command,value){
+    console.log(value + "got value")
+    commandDisplay.append(commandPrefix + value + "<br><br>")
     commands[command].map((line) => {
         console.log(line)
         commandDisplay.append(line + "<br>")
     })
+    commandDisplay.append("<br>")
 }
 
 function prepare_commandBox(){
     commandBox.focus()
     commandBox.get(0).scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
 }
+
+print_command("motd","motd")
