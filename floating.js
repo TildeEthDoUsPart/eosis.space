b8c0c6049a9352c3be1d231034c5b0b036a21256
Antoine Cerdan <antoinecerdan31@gmail.com>
2024-01-16 15:07:12 +0100

    💡style: add comments and delimiters

--- floating.js
+++ floating.js
@@ -2,11 +2,10 @@ let notches = $('.notch')
 let tooltip = $('#tooltip')
 
 for (i=0;i<notches.length;i++){
-    console.log(notches[i]) 
     notches[i].addEventListener("mouseover",changeTooltip)
 }
 
-function changeTooltip(evt){
+function changeTooltip(evt){ // Changing tooltip text on icon hover
     let text = evt.target.getAttribute('data-tooltip')
     tooltip.text(text)
 }
