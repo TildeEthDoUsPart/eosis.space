6a5fe0b794be9297e57e7eadf9214c6194d6cdc2
Antoine Cerdan <antoinecerdan31@gmail.com>
2024-01-18 11:31:46 +0100

    🐛fix: issue fixed, removing console.logs

--- floating.js
+++ floating.js
@@ -10,7 +10,6 @@ let mobileClicked = ""
 
 function changeTooltip(evt){ // Changing tooltip text on icon hover
     let text = evt.target.getAttribute('data-tooltip')
-    alert('changed from changeTooltip called from eventListener')
     tooltip.text(text)
 }
 
@@ -25,12 +24,8 @@ if (isMobile()){ // On mobile, the link has to be tapped two times to trigger re
     for (i=0;i<links.length;i++){
         links[i].addEventListener("click",(evt) => { // If the tooltip is equal to the link the user clicked, that means it is the last link pressed by user (double tap simulation)
             if (tooltip.text() === evt.target.getAttribute('data-tooltip')){
-                alert(tooltip.text())
-                alert(evt.target.getAttribute('data-tooltip'))
-                alert("tooltip is equal so we change")
                 window.location.replace(evt.getAttribute('href'));
             }
-            alert("prevented and changed tooltip")
             evt.preventDefault()
             changeTooltip(evt)
 
